cedb8a63c01e9f03ddf2596d2c9d9c457501e5ee
HanadiSabarnah <hsabarnah@gmail.com>
2020-12-03 17:39:50 +0200

    navbar and product

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,29 +2,37 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import SignInPage from './Auth/Signinpage';
 import SignUpPage from './Auth/Signupage';
-// import Homepage from './Components/Homepage/Homepage';
+import Homepage from './Components/Homepage/Homepage';
+import Product from './Components/Homepage/Products/product';
+import ProductTools from './Components/Homepage/Products/tools';
+import ProductMaterials from './Components/Homepage/Products/materials';
 
 // import { connect } from 'react-redux'
 // import { Dispatch } from 'redux'
 // import { AppState } from './store/rootStore'
 // import 'reset-css'
-import { makeStyles } from "@material-ui/core/styles";
+// import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: theme.spacing(3),
-  }
-}))
+// const useStyles = makeStyles((theme) => ({
+//   container: {
+//     padding: theme.spacing(3),
+//   }
+// }))
 
 
 const App = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
     // <Homepage />
-    <div className={classes.container}>
+    <div >
       <Router>
         <Switch>
-          {/* <Route exact from="/" render={props => <Home {...props} />} /> */}
+          <Route exact path="/" render={()=> <Homepage />} />        
+                   
+        <Route path="/product" exact component={Product} />
+        <Route path="/tools" exact component={ProductTools} />
+        <Route path="/materials" exact component={ProductMaterials} />
+        
           <Route exact path="/signup" render={() => <SignUpPage />} />
           <Route exact path="/signin" render={() => <SignInPage />} />
         </Switch>
--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+
+import Navbar from './Navbar/Navbar'
 
 const Homepage = (props: any) => {
     return (
-        <div>
-            Hello from HomePage
+
+
+
+        <div className="homepage">
+            <Navbar />
         </div>
+
     )
 
 }
