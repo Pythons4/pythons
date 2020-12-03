dd7413e32ccbd74b2899588bcbff514ede03fb78
QamarJaradat <moon19.qs@gmail.com>
2020-12-03 15:21:46 +0200

    connect with MongoDB

--- backend/polls/views.py
+++
@@ -1,9 +0,0 @@
-# from django.shortcuts import render
-
-# Create your views here.
-import HttpResponse from django.http
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 import SignInPage from './Auth/Signinpage';
 import SignUpPage from './Auth/Signupage';
-// import Homepage from './Components/Homepage/Homepage';
+import 'antd/dist/antd.css';
 
 // import { connect } from 'react-redux'
 // import { Dispatch } from 'redux'
 // import { AppState } from './store/rootStore'
 // import 'reset-css'
 import { makeStyles } from "@material-ui/core/styles";
+import Homepage from './Components/Homepage/Homepage';
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -21,8 +22,9 @@ const App = () => {
   const classes = useStyles();
   return (
     // <Homepage />
-    <div className={classes.container}>
+    <div className='App'>
       <Router>
+        <Homepage />
         <Switch>
           {/* <Route exact from="/" render={props => <Home {...props} />} /> */}
           <Route exact path="/signup" render={() => <SignUpPage />} />
