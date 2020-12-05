5b43d4ac28746e42428d5bed74241b733ea58398
QamarJaradat <moon19.qs@gmail.com>
2020-12-05 16:04:09 +0200

    test

--- backend/env/Scripts/django-admin.py
+++ backend/env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!C:\Users\Asus\Desktop\RBK\Immersive\final\pythons\backend\env\Scripts\python.exe
+#!c:\users\asus\desktop\rbk\immersive\final\pythons\backend\env\scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
