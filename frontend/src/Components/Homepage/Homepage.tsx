78a4638d6a944de84fe6c542a485d6c755c98b72
HanadiSabarnah <hsabarnah@gmail.com>
2020-12-10 12:18:47 +0200

    userprofile

--- frontend/src/Components/Homepage/Homepage.tsx
+++ frontend/src/Components/Homepage/Homepage.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button';
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import './Homepage.css'
+import Contaner from './section'
+import Footer from './Footer'
 
 
 
@@ -9,13 +11,25 @@ import './Homepage.css'
 
 const Homepage = (props: any) => {
     return (
-        <div className='container-img'>
+        <>
+            <div className='container-img'>
 
-            <p>Cleaning is hard at first, messy in the middle and gorgeous in the end</p>
-            <div className='container-btns'>
-                <Button color="primary" variant="contained" >Book NOW</Button>
+                <p>Cleaning is hard at first, messy in the middle and gorgeous in the end</p>
+
+                <div className='container-btns'>
+                    <Link to="/tips" style={{ textDecoration: "none" }}>
+                        <Button style={{ marginLeft: "8px" }} color="primary" variant="contained" size="large">Book Now</Button>
+                    </Link>
+                </div>
             </div>
-        </div>
+
+            <div>
+                <Contaner />
+            </div>
+            <div>
+                <Footer />
+            </div>
+        </>
 
 
 
