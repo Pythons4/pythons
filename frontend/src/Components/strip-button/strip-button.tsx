b217cd399179f4f72af5f6c8ee6c5cb0379a24ee
belal-abuhani <belal.abuhani@gmail.com>
2020-12-12 20:32:10 +0200

    90% done of payment system products

--- frontend/src/Components/strip-button/strip-button.tsx
+++ frontend/src/Components/strip-button/strip-button.tsx
@@ -1,20 +1,31 @@
 import react from "react";
 import StripeCheckOut from "react-stripe-checkout";
+import buyWhatInCart from '../../store/actions/buywhatincart';
+import store from '../../store';
+
+
 
 interface Props {
+
     price: any
 }
 
 function StripeCheckOutButton(props: Props) {
+    let { whatincart } = store.getState().cartReducer;
+
     let priceStripe = props.price * 100;
     let key = "pk_test_51HxYRdICrWL5m2FNWABUKvBtta8W65FOeAJhls6MBFGtqLu1NPTVSD0PlMe9k8sDa1B5bLhr6Pus9XNpDdMS0vID003qTitFsD"
     let onToken = (token: any) => {
+        store.dispatch(buyWhatInCart(whatincart));
+
         console.log(token);
+        console.log(whatincart);
+
         alert("payment sucsse");
     }
     return (
         <StripeCheckOut
-            label="Pay Now"
+            label="confirm && Pay Now"
             name="Products"
             shippingAddress
             billingAddress
