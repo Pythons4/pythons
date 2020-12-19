3812ec7105957ae80091b5e7db2e8740f5154f62
QamarJaradat <moon19.qs@gmail.com>
2020-12-19 14:59:26 +0200

    admin signin page layout done

--- frontend/src/store/reducers/adminreduser.ts
+++ frontend/src/store/reducers/adminreduser.ts
@@ -4,6 +4,7 @@ const userReducer = (state: any, action: any) => {
             localStorage.setItem('adminToken', JSON.stringify(action.adminToken))
             localStorage.setItem('adminid', JSON.stringify(action.adminId._id))
             localStorage.setItem('admininfo', JSON.stringify(action.adminId))
+            window.location.href = '/adminprofile'
 
             return {
                 adminToken: localStorage.getItem('adminToken'), adminid: localStorage.getItem('adminid'),
@@ -14,12 +15,17 @@ const userReducer = (state: any, action: any) => {
             localStorage.removeItem('adminToken')
             localStorage.removeItem('adminid')
             localStorage.removeItem('admininfo')
+            window.location.href = '/homepage'
+
             return {
                 adminToken: localStorage.getItem('adminToken'), adminid: localStorage.getItem('adminid'),
                 admininfo: localStorage.getItem('admininfo')
             }
 
-
+        case 'SIGN-IN_ERROR':
+            return {
+                error: action.value
+            }
         default:
             return {
                 adminToken: localStorage.getItem('adminToken'), adminid: localStorage.getItem('adminid'),
